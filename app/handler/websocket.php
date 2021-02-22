ba9e7088e18b31e072845a1dc4485adcadc803d4
lucasfoussier <lucasfoussier@gmail.com>
2021-02-22 20:01:43 +0100

    Feature: websocket

--- app/handler/websocket.php
+++ app/handler/websocket.php
@@ -8,11 +8,44 @@ use Bref\Websocket\SimpleWebsocketClient;
 use AsyncAws\DynamoDb\Input\PutItemInput;
 use AsyncAws\DynamoDb\ValueObject\AttributeValue;
 use AsyncAws\DynamoDb\DynamoDbClient;
+use Bref\Symfony\Messenger\Service\Sqs\SqsConsumer;
+use Symfony\Component\Dotenv\Dotenv;
 
-require __DIR__ . '/../vendor/autoload.php';
+//require __DIR__ . '/../vendor/autoload.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
 class MyHandler extends WebsocketHandler
 {
+
+
+    public function __construct()
+    {
+        if(!isset($_SERVER['APP_ENV'])){
+            (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+        } else {
+            if($_SERVER['APP_ENV'] !== 'prod') {
+                (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+            }
+        }
+
+        if(isset($_SERVER['APP_STAGE'])){
+            define('APP_STAGE', $_SERVER['APP_STAGE']);
+        } else {
+            throw new \Exception('APP_STAGE undefined');
+        }
+
+        if(isset($_SERVER['APP_NAME'])){
+            define('APP_NAME', $_SERVER['APP_NAME']);
+        } else {
+            throw new \Exception('APP_NAME undefined');
+        }
+
+        define('DYNAMODB_TABLE_PREFIX', APP_NAME.'.'.APP_STAGE.'.');
+
+        $kernel = new \App\Kernel($_SERVER['APP_ENV'], (bool)$_SERVER['APP_DEBUG']);
+        $kernel->boot();
+
+    }
 
     public function handleWebsocket(WebsocketEvent $event, Context $context): HttpResponse
     {
--- app/src/Entity/WebsocketPool.php
+++ app/src/Entity/WebsocketPool.php
@@ -46,6 +46,69 @@ class WebsocketPool extends Item
     ]
     private string $stage;
 
+    /**
+     * @return string
+     */
+    public function getConnectionId(): string
+    {
+        return $this->connectionId;
+    }
+
+    /**
+     * @param string $connectionId
+     */
+    public function setConnectionId(string $connectionId): void
+    {
+        $this->connectionId = $connectionId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiId(): string
+    {
+        return $this->apiId;
+    }
+
+    /**
+     * @param string $apiId
+     */
+    public function setApiId(string $apiId): void
+    {
+        $this->apiId = $apiId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRegion(): string
+    {
+        return $this->region;
+    }
+
+    /**
+     * @param string $region
+     */
+    public function setRegion(string $region): void
+    {
+        $this->region = $region;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStage(): string
+    {
+        return $this->stage;
+    }
+
+    /**
+     * @param string $stage
+     */
+    public function setStage(string $stage): void
+    {
+        $this->stage = $stage;
+    }
 
 
 }
